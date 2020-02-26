7a957821ace9adcf51c55d0d379cb2035f024a45
Nisan Itzhakov <nisan.mfy@gmail.com>
2020-02-26 11:50:14 +0200

    add lightswitch api

---
+++ pkg/apis/lightswitch/group.go
@@ -0,0 +1,6 @@
+// Package lightswitch contains lightswitch API versions.
+//
+// This file ensures Go source parsers acknowledge the lightswitch package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package lightswitch
